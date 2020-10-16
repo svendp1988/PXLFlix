42b5466df50fa66c8ead67dc0f8b5f239d40dc3c
Sven De Potter <sven.depotter@hotmail.com>
2020-10-16 22:00:40 +0200

    Working on browse-page

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,9 +1,29 @@
 import React from 'react'
-import { Background, Container, Logo, ButtonLink } from './styles/header'
+import {
+    Text,
+    Feature,
+    FeatureCallOut,
+    Background,
+    Container,
+    Logo,
+    ButtonLink
+} from './styles/header'
 import { Link as ReactRouterLink } from 'react-router-dom';
 
 export default function Header({ bg = true, children, ...restProps }) {
     return bg ? <Background {...restProps}>{children}</Background> : children;
+};
+
+Header.Feature = function HeaderFeature({ children, ...restProps }) {
+    return <Feature {...restProps}>{children}</Feature>;
+};
+
+Header.Text = function HeaderText({ children, ...restProps }) {
+    return <Text {...restProps}>{children}</Text>;
+};
+
+Header.FeatureCallOut = function HeaderFeatureCallOut({ children, ...restProps }) {
+    return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>;
 };
 
 Header.Frame = function HeaderFrame({ children, ...restProps }) {
--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,12 +1,49 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import { SelectProfileContainer } from './profiles';
 import { FirebaseContext } from '../context/firebase';
+import { Header, Loading } from '../components';
+import * as ROUTES from '../constants/routes';
+import logo from '../logo.svg'
 
 export function BrowseContainer({ slides }) {
     const [profile, setProfile] = useState({});
+    const [loading, setLoading] = useState(true);
     const { firebase } = useContext(FirebaseContext);
     const user = firebase.auth().currentUser || {};
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 3000);
+    }, [profile.displayName]);
+
     return (
-        <SelectProfileContainer user={user} setProfile={setProfile} />
+        profile.displayName
+            ? (<React.Fragment>
+                {
+                    loading
+                        ? (<Loading src={user.photoURL} />)
+                        : (<Loading.ReleaseBody />)
+                }
+                <Header src="joker1">
+                    <Header.Frame>
+                        <Header.Logo to={ROUTES.HOME} src={logo} alt="PXL Flix" />
+                    </Header.Frame>
+                    <Header.Feature>
+                        <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
+                        <Header.Text>
+                            Forever alone in a crowd, failed comedian Arthur Fleck
+                            seeks
+                            connection as he walks the streets of Gotham City, Arthur
+                            wears two masks -- the one he paints for his day job as a clow,,
+                            and the guise
+                            he projects in a futile attempt to feel like he's part of
+                            the world
+                            around him.
+                        </Header.Text>
+                    </Header.Feature>
+                </Header>
+            </React.Fragment>)
+            : (<SelectProfileContainer user={user} setProfile={setProfile} />)
     );
-}
+}  
